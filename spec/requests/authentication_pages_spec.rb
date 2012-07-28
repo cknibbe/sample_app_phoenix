bf8ed8769e662943c80f03ca1bb3186cb9897e18
Clo Knibbe <clo@phoenix>
2012-07-28 16:41:59 -0400

    Refactor specs

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -6,23 +6,21 @@ describe "Authentication" do
 
   describe "signin page" do
     before { visit signin_path }
-
-    it { should have_selector('h1',    text: 'Sign in') }
-    it { should have_selector('title', text: 'Sign in') }
+    it { should have_banner('Sign in', 'Sign in')}
   end
   
   describe "signin" do
     before { visit signin_path }
 
     describe "with invalid information" do
-      before { click_button "Sign in" }
+      before { invalid_signin }
 
       it { should have_selector('title', text: 'Sign in') }
       it { should have_error_message('Invalid') }
  
       describe "after visiting another page" do
         before { click_link "Home" }
-        it { should_not have_selector('div.alert.alert-error') }
+        it { should_not have_error_message }
       end
     end
     
@@ -36,7 +34,7 @@ describe "Authentication" do
       it { should_not have_link('Sign in', href: signin_path) }
       
       describe "followed by signout" do
-        before { click_link "Sign out" }
+        before { signout }
         it { should have_link('Sign in') }
       end
     end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,8 +6,23 @@ def valid_signin(user)
   click_button "Sign in"
 end
 
+def invalid_signin
+  click_button "Sign in"
+end
+
+def signout
+  click_link "Sign out"
+end
+
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_banner do |h1, title|
+  match do |page|
+    page.should have_selector('h1',    text: h1)
+    page.should have_selector('title', text: title)
   end
 end
